efeb11cbb24a78ebc8c5f157ab7b9e449e17ad33
Loksy0_ <ultrablaster591@gmail.com>
2025-08-12 14:27:11 +0200

    Create __init__.py

---
+++ module/__init__.py
@@ -0,0 +1,11 @@
+from .ip_info import full_ip_report, ping
+from .port_scan import scan_target
+from .sub_check import check_subdomains
+from .cms_admin_check import cms_admin_check
+from .cert_info import ssl_info
+from .web_reconnaissance import web_app_recon
+from .public_leaks import leaks_check
+
+__version__ = "1.0 Release"
+__date__ = "12.08.2025"
+__author__ = "Loksy0"
