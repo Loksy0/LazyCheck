4a992608b3dba3308466a5b0fba4e745c7fff00c
Loksy0_ <ultrablaster591@gmail.com>
2025-08-12 14:25:49 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,23 +8,29 @@ if [ ! -f "$CONFIG_FILE" ]; then
     exit 1
 fi
 
-API_KEY=$(grep -oP '"api_key"\s*:\s*"\K[^"]+' "$CONFIG_FILE")
+VIEWDNS_KEY=$(grep -oP '"viewdns\.info_api_key"\s*:\s*"\K[^"]+' "$CONFIG_FILE")
+LEAKLOOKUP_KEY=$(grep -oP '"leak-lookup\.com_api_key"\s*:\s*"\K[^"]+' "$CONFIG_FILE")
 
-if [[ -z "$API_KEY" || "$API_KEY" == "enter_here_your_api_key" ]]; then
-    echo "Field api_key w $CONFIG_FILE is wrong!"
+if [[ -z "$VIEWDNS_KEY" || "$VIEWDNS_KEY" == "enter_here_your_api_key" ]]; then
+    echo "Pole viewdns.info_api_key w $CONFIG_FILE jest niepoprawne lub puste!"
+    exit 1
+fi
+
+if [[ -z "$LEAKLOOKUP_KEY" || "$LEAKLOOKUP_KEY" == "enter_here_your_api_key" ]]; then
+    echo "Pole leak-lookup.com_api_key w $CONFIG_FILE jest niepoprawne lub puste!"
     exit 1
 fi
 
 if [ -f "$REQUIREMENTS_FILE" ]; then
     pip install -r "$REQUIREMENTS_FILE"
 else
-    echo "No file: $REQUIREMENTS_FILE"
+    echo "Brak pliku: $REQUIREMENTS_FILE"
     exit 1
 fi
 
 if ! command -v nmap &> /dev/null; then
-    echo "You have to install nmap!"
+    echo "Musisz zainstalować nmap!"
     exit 1
 else
-    echo "nmap is installed."
+    echo "nmap jest zainstalowany."
 fi
